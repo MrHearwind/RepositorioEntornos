e6fdf10175cc212c8994d51572d862573934a2a7
nico <nico@DESKTOP-5LDHQ6F>
2026-02-25 08:56:39 +0100

    Version con constructor y metodo hipotenusa

--- ProyectoGithub/src/proyecto/proyectoGit.java
+++ ProyectoGithub/src/proyecto/proyectoGit.java
@@ -4,21 +4,39 @@ import java.util.Scanner;
 
 public class proyectoGit {
 
+	private double a;
+	private double b;
+
+	public proyectoGit(double a, double b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+
+	public double hipotenusa() {
+
+		double hipotenusa;
+
+		hipotenusa = Math.sqrt(Math.pow(this.a, 2) + Math.pow(this.b, 2));
+
+		return hipotenusa;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner teclado=new Scanner(System.in);
-		
-		double ladoA,ladoB,hipotenusa;
-		
+		Scanner teclado = new Scanner(System.in);
+		proyectoGit triangulo;
+		double ladoA, ladoB, hipotenusa;
+
 		System.out.println("Escribe Lado A: ");
-		ladoA=teclado.nextDouble();
+		ladoA = teclado.nextDouble();
 		System.out.println("Escribe Lado B: ");
-		ladoB=teclado.nextDouble();
-		
-		hipotenusa=Math.sqrt(Math.pow(ladoA,2)+Math.pow(ladoB, 2));
-		
-		
+		ladoB = teclado.nextDouble();
+
+		triangulo = new proyectoGit(ladoA, ladoB);
+
+		hipotenusa=	triangulo.hipotenusa();
 		System.out.println("La hipotenusa es: "+hipotenusa);
 		
 		
